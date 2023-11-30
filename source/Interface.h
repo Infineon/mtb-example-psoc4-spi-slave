9464505ae797c02580b037ae56b175095ce24bdf
gitlab-runner <do-not-reply@cypress.com>
2023-11-30 08:04:42 -0600

    Upload mtb-example-psoc4-spi-slave [2019]

--- source/Interface.h
+++ source/Interface.h
@@ -4,7 +4,7 @@
 * Description: This file contains all the macros for SPI interface
 *
 *******************************************************************************
-* Copyright 2021-2022, Cypress Semiconductor Corporation (an Infineon company) or
+* Copyright 2021-2023, Cypress Semiconductor Corporation (an Infineon company) or
 * an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
 *
 * This software, including source code, documentation and related
